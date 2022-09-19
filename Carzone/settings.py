62367afea763be019020b608ce466602b9cf983e
Gio Elv <giolom548@gmail.com>
2022-09-19 15:00:42 +0400

    creates pages and configured navigation links

--- Carzone/settings.py
+++ Carzone/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_URL = True
+
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -119,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Carzone/static'),
+]
