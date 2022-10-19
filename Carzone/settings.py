9c13426ebad5ae163053f7c0c92a137241a9e972
Gio Elv <giolom548@gmail.com>
2022-10-19 12:34:38 +0400

    started deployment

--- Carzone/settings.py
+++ Carzone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -93,15 +94,15 @@ WSGI_APPLICATION = 'Carzone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'carzone_db',
-        'USER':'postgres',
-        'PASSWORD':'giolom1994',
-        'HOST':'localhost',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME':'carzone_db',
+#        'USER':'postgres',
+#        'PASSWORD':'giolom1994',
+#        'HOST':'localhost',
+#    }
+#}
 
 
 # Password validation
